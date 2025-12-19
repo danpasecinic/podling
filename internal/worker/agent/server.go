07187372f782a8013f2ba9ece9b01d9a376a5444
danpasecinic <plam.developer@gmail.com>
2025-12-19 19:19:48 +0100

    feat: add routes for streaming

--- internal/worker/agent/server.go
+++ internal/worker/agent/server.go
@@ -26,9 +26,11 @@ func (s *Server) RegisterRoutes(e *echo.Echo) {
 	v1.POST("/tasks/:id/execute", s.ExecuteTask)
 	v1.GET("/tasks/:id/status", s.GetTaskStatus)
 	v1.GET("/tasks/:id/logs", s.GetTaskLogs)
+	v1.GET("/tasks/:id/logs/stream", s.StreamTaskLogs)
 
 	v1.POST("/pods/:id/execute", s.ExecutePod)
 	v1.GET("/pods/:id/status", s.GetPodStatus)
 	v1.GET("/pods/:id/logs", s.GetPodLogs)
+	v1.GET("/pods/:id/logs/stream", s.StreamPodLogs)
 	v1.DELETE("/pods/:id", s.DeletePod)
 }
